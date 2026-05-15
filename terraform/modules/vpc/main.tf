9d782f296e9dc4abb71e6f1183bb2bb28e3de34d
Adam Ronald Gray <82571100+agray998@users.noreply.github.com>
2026-05-15 10:13:31 +0100

    Add VPC, Internet Gateway, and Route Table resources

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -1,1 +1,30 @@
+resource "aws_vpc" "project_vpc" {
+  cidr_block = "${var.cidr_prefix}/16"
+  tags       = {
+    Name = "Project VPC"
+  }
+}
 
+resource "aws_internet_gateway" "project_gw" {
+  vpc_id = aws_vpc.project_vpc.id
+  tags   = {
+    Name = "Project IGW"
+  }
+}
+
+resource "aws_route_table" "project_rt" {
+  vpc_id = aws_vpc.project_vpc.id
+
+  route {
+      cidr_block = "0.0.0.0/0"
+      gateway_id = aws_internet_gateway.project_gw.id
+    }
+
+  route {
+      ipv6_cidr_block = "::/0"
+      gateway_id      = aws_internet_gateway.project_gw.id
+    }
+  tags = {
+    Name = "Project RT"
+  }
+}
