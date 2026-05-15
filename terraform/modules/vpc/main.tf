3d816b09085037a0d63b794d46d63c6874af3801
Adam Ronald Gray <82571100+agray998@users.noreply.github.com>
2026-05-15 12:56:47 +0100

    Refactor security groups for CI and deployment
    Replaced the 'allow_ssh' security group with 'ci' security group for CI server traffic. Added ingress rules for MySQL, Jenkins, and HTTP, and created a new 'deploy' security group with specific ingress and egress rules.

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -29,9 +29,9 @@ resource "aws_route_table" "project_rt" {
   }
 }
 
-resource "aws_security_group" "allow_ssh" {
-  name        = "allow_ssh"
-  description = "Allow SSH inbound traffic"
+resource "aws_security_group" "ci" {
+  name        = "ci_sg"
+  description = "Allow necessary inbound traffic for CI server"
   vpc_id      = aws_vpc.project_vpc.id
 
   ingress = [
@@ -39,6 +39,39 @@ resource "aws_security_group" "allow_ssh" {
       description      = "SSH"
       from_port        = 22
       to_port          = 22
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
+    },
+    {
+      description      = "MySQL"
+      from_port        = 3306
+      to_port          = 3306
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
+    },
+    {
+      description      = "Jenkins"
+      from_port        = 8080
+      to_port          = 8080
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
+    },
+    {
+      description      = "HTTP"
+      from_port        = 80
+      to_port          = 80
       protocol         = "tcp"
       cidr_blocks      = ["0.0.0.0/0"]
       ipv6_cidr_blocks = []
@@ -63,6 +96,66 @@ resource "aws_security_group" "allow_ssh" {
   ]
 
   tags = {
-    Name = "Project SG"
+    Name = "Project CI SG"
+  }
+}
+
+resource "aws_security_group" "deploy" {
+  name        = "deploy_sg"
+  description = "Allow necessary deploy server inbound"
+  vpc_id      = aws_vpc.project_vpc.id
+
+  ingress = [
+    {
+      description      = "SSH"
+      from_port        = 22
+      to_port          = 22
+      protocol         = "tcp"
+      cidr_blocks      = []
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      security_groups  = [aws_security_group.ci.id]
+      self             = false
+    },
+    {
+      description      = "MySQL"
+      from_port        = 3306
+      to_port          = 3306
+      protocol         = "tcp"
+      cidr_blocks      = []
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = true
+    },
+    {
+      description      = "HTTP"
+      from_port        = 80
+      to_port          = 80
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
+    }
+  ]
+
+  egress = [
+    {
+      description      = "egress"
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
+    }
+  ]
+
+  tags = {
+    Name = "Project Deploy SG"
   }
 }
