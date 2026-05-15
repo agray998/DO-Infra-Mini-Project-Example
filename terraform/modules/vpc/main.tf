0f90ba58ec66d04c4d128647a35b082fbcd1358a
Adam Ronald Gray <82571100+agray998@users.noreply.github.com>
2026-05-15 10:49:55 +0100

    Add security group to allow SSH traffic

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -28,3 +28,41 @@ resource "aws_route_table" "project_rt" {
     Name = "Project RT"
   }
 }
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = aws_vpc.project_vpc.id
+
+  ingress = [
+    {
+      description      = "SSH"
+      from_port        = 22
+      to_port          = 22
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
+    }
+  ]
+
+  egress = [
+    {
+      description      = "egress"
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
+    }
+  ]
+
+  tags = {
+    Name = "Project SG"
+  }
+}
