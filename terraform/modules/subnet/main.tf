a73ab31d2546527b56cdf9355a76f3b5c07b9ebf
Adam Ronald Gray <82571100+agray998@users.noreply.github.com>
2026-05-15 10:16:26 +0100

    Add AWS subnet and route table association resources

--- terraform/modules/subnet/main.tf
+++ terraform/modules/subnet/main.tf
@@ -1,1 +1,14 @@
+resource "aws_subnet" "project_sub" {
+  vpc_id                  = var.vpc_id
+  cidr_block              = "${var.cidr_prefix}/24"
+  availability_zone       = var.subnet_az
+  map_public_ip_on_launch = true
+  tags                    = {
+    Name = var.subnet_name
+  }
+}
 
+resource "aws_route_table_association" "project_rta" {
+  subnet_id      = aws_subnet.project_sub.id
+  route_table_id = var.rt_id
+}
