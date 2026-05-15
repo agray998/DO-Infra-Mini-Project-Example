327ff1cfbb8fc07cd0cc3058a678fa9fafb0b272
Adam Ronald Gray <82571100+agray998@users.noreply.github.com>
2026-05-15 10:08:05 +0100

    setup ec2/main.tf

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -1,1 +1,16 @@
+resource "aws_instance" "ec2_instance" {
+    ami               = var.ami_id
+    instance_type     = "t3.micro"
+    key_name          = "demo-11-05-2026"
 
+
+    network_interface {
+      device_index         = 0
+      network_interface_id = aws_network_interface.instance_nic.id
+    }
+}
+
+resource "aws_network_interface" "instance_nic" {
+  subnet_id       = var.subnet_id
+  security_groups = [var.sg_id]
+}
