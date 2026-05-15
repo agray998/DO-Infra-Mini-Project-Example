622cd99923cf8a17f7aece1b068de1b4da2f072f
Adam Ronald Gray <82571100+agray998@users.noreply.github.com>
2026-05-15 10:43:08 +0100

    Add subnet_count variable to variables.tf
    Added subnet_count variable with a default value of 3.

--- terraform/modules/subnet/variables.tf
+++ terraform/modules/subnet/variables.tf
@@ -2,9 +2,11 @@ variable "vpc_id" {}
 
 variable "cidr_prefix" {}
 
-variable "subnet_az" {}
-
 variable "rt_id" {}
+
+variable "subnet_count" {
+  default = 3
+}
 
 variable "zones" {
   default = ["a", "b", "c"]
