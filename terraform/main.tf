70c7cefa0c3c34a48640de6f2ce491c9bf8153b4
Adam Ronald Gray <82571100+agray998@users.noreply.github.com>
2026-05-15 13:02:50 +0100

    Configure PublicSubnets module with parameters

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,10 @@
-module "PublicSubnets" {}
+module "PublicSubnets" {
+  source = "modules/subnet"
+  vpc_id = aws_vpc.project_vpc.id
+  cidr_prefix = "10.0"
+  rt_id = aws_route_table.project_rt.id
+  subnet_count = 2
+}
 
 module "CI" {}
 
