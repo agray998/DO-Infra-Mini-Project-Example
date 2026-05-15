7fba852683e4d2a1da296e92889429333b74c9ac
Adam Ronald Gray <82571100+agray998@users.noreply.github.com>
2026-05-15 13:05:22 +0100

    Add RDS instance resource configuration

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -1,1 +1,11 @@
-
+resource "aws_db_instance" "project_db" {
+  allocated_storage    = 10
+  db_name              = "projectdb"
+  engine               = "mysql"
+  engine_version       = "8.0"
+  instance_class       = "db.t3.micro"
+  username             = "foo"
+  password             = var.rds_pass
+  parameter_group_name = "default.mysql8.0"
+  skip_final_snapshot  = true
+}
